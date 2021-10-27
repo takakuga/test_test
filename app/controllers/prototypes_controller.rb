f5468325346ee008f78353bafc77c586ec4358f3
takakuga <sazanami398@gmail.com>
2021-10-27 22:02:44 +0900

    ストロングパラメーターを設定

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -9,6 +9,12 @@ class PrototypesController < ApplicationController
   end
 
   def create
+    Prototype.create(prototype_params)
   end
+
+  private
+  def prototype_params
+    params.require(:prototype).premit(:title, :catch_copy, :concept, :image).marge(user_id: current_user.id)
+
 
 end
