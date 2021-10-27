92cbefb2e618c033229471a5461fc276285adfa3
takakuga <sazanami398@gmail.com>
2021-10-27 14:48:55 +0900

    newアクションを定義

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,12 +1,14 @@
 class PrototypesController < ApplicationController
   
   def index
+    @prototypes = Prototype.all
   end
 
   def new
+    @prototype = Prototype.new
   end
 
   def create
   end
-  
+
 end
